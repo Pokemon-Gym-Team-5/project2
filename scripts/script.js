a28814893589cf6d99fbe15a3cccf8f8df543188
Solomon <solo.serry@gmail.com>
2022-01-06 21:13:23 -0500

    updated some styles :fire:

--- scripts/script.js
+++ scripts/script.js
@@ -30,12 +30,14 @@ window.addEventListener("DOMContentLoaded", () => {
     typeElementHolder.innerHTML = placeholder.type
     abilitiesElementHolder.innerHTML = placeholder.abilities
 
+
     pokemonApp.img.appendChild(imgElementHolder);
     pokemonApp.name.appendChild(nameElementHolder);
     pokemonApp.type.appendChild(typeElementHolder);
     pokemonApp.abilities.appendChild(abilitiesElementHolder);
-
-   
+    
+    
+    
 });
 
 pokemonApp.getAllPokemon = () => {
@@ -43,7 +45,7 @@ pokemonApp.getAllPokemon = () => {
         return res.json()
     }).then((jsonData) => {
         pokemonApp.getRandom(jsonData.results)
-
+        
     });
 };
 
@@ -56,40 +58,42 @@ pokemonApp.getRandom = (jsonResults1) => {
         }).then((jsonData) => {
             pokemonApp.displayPokemon(jsonData)
         });
+        // pokemonApp.img.classList.add('fade')
         pokemonApp.img.innerHTML = '';
         pokemonApp.name.innerHTML = '';
         pokemonApp.type.innerHTML = '';
         pokemonApp.abilities.innerHTML = '';
-
+        
     });
 };
 
 pokemonApp.displayPokemon = (jsonResults2) => {
-
+    
     const imgElement = document.createElement('img')
     const nameElement = document.createElement('h2')
-
+    
     const arrayListOfType = jsonResults2.types
     arrayListOfType.forEach((arrayTypes) => {
         const typeElement = document.createElement('li')
         typeElement.innerHTML = arrayTypes.type.name
         pokemonApp.type.appendChild(typeElement);
     });
-
+    
     const arrayListOfAbilities = jsonResults2.abilities;
     arrayListOfAbilities.forEach((arrayAbilities) => {
         const abilitiesElement = document.createElement('li')
         abilitiesElement.innerHTML = arrayAbilities.ability.name
         pokemonApp.abilities.appendChild(abilitiesElement);
     });
-
+    
     imgElement.src = jsonResults2.sprites.other['official-artwork'].front_default
     imgElement.alt = `photo of ${jsonResults2.name}`
     nameElement.innerHTML = jsonResults2.name
-
+    
     pokemonApp.img.appendChild(imgElement);
     pokemonApp.name.appendChild(nameElement);
-
+    // pokemonApp.img.classList.remove('fade')
+    
 };
 
 pokemonApp.init = () => {
